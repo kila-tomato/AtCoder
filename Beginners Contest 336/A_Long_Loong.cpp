5e5cb37f18ea66e311082f9320edec1bb2095e15
kila-tomato <l_c1oud.s_vii31@icloud.com>
2024-01-15 20:42:31 +0900

    336A模範回答

--- Beginners Contest 336/A_Long_Loong.cpp
+++ Beginners Contest 336/A_Long_Loong.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+#define rep(i,n) for (int i = 0; i < (n); i++)
 
 int main(){
-  std::string o = "";
-	int N;
-	cin >> N ;
-
-  while(N>0){
-    o.insert(o.end(), 'o');
+//  std::string o = "";
+	int n;
+	cin >> n ;
+        cout << "L" << string(n,'o') << "ng\n";
+//  while(N>0){
+//    o.insert(o.end(), 'o');
     //o.push_back('o');
-    N--;
-  }
-	cout << "L" << o << "ng" << endl;
+//    N--;
+//  }
+//	cout << "L" << o << "ng" << endl;
 	return 0;
 }
 
